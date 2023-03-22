bfaf84f1b04e42b41518826a7175f938f58f8884
CrashMP <128427983+CrashMP@users.noreply.github.com>
2023-03-22 18:40:50 -0300

    Adição de "senha"
     Adição de uma senha com o objetivo de meramente testar essa função, nada complexo

--- Cartorio.c
+++ Cartorio.c
@@ -114,51 +114,73 @@ int main() //Definindo variáveis - função principal
 {
 	int opcao=0;
 	int laco=1;
+	char senhadigitada[20]="a";
+	int comparacao;
 	
-	for(laco=1;laco=1;)
+	
+	printf("### Cartório da EBAC ###\n\n");
+	printf("Login de administrador!\n\nDigite sua senha: ");
+	scanf("%s",senhadigitada);
+	
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0)
 	{
-		
-		system("cls");
-			
-    	setlocale(LC_ALL, "Portuguese"); //É necessário colocar em pt para a utilização de acentos
+		system ("cls");
 	
-    	printf("### Cartório da EBAC ###\n\n");
-    	printf("Escolha a opção desejada do menu:\n\n");
-    	printf("\t1 - Registrar nomes\n"); //BarraT é parágrafo
-    	printf("\t2 - Consultar nomes\n");
-    	printf("\t3 - Deletar nomes\n\n");
-    	printf("\t4 - Sair do sistema\n\n");
-    	printf("Opção: "); //fim do menu
-	
-    	scanf ("%d", &opcao); //armazenando escolha do usuário
-	
-    	system("cls"); //Responsável por limpar a tela
-    	
-    	
-    	switch(opcao) //Início da seleção do menu
+    	for(laco=1;laco=1;)
     	{
-    		case 1:
-    		registro(); // Chamada de funções (as 3 abaixo são a mesma coisa)
-	    	break;
+		
+	    	system("cls");
+			
+        	setlocale(LC_ALL, "Portuguese"); //É necessário colocar em pt para a utilização de acentos
+	
+    	    printf("### Cartório da EBAC ###\n\n");
+        	printf("Escolha a opção desejada do menu:\n\n");
+        	printf("\t1 - Registrar nomes\n"); //BarraT é parágrafo
+        	printf("\t2 - Consultar nomes\n");
+        	printf("\t3 - Deletar nomes\n\n");
+        	printf("\t4 - Sair do sistema\n\n");
+        	printf("Opção: "); //fim do menu
+	
+        	scanf ("%d", &opcao); //armazenando escolha do usuário
+	
+        	system("cls"); //Responsável por limpar a tela
+    	
+    	
+        	switch(opcao) //Início da seleção do menu
+        	{
+        		case 1:
+        		registro(); // Chamada de funções (as 3 abaixo são a mesma coisa)
+	        	break;
 	    	
-	    	case 2:
-	    	consulta();
-    		break;
+	        	case 2:
+	        	consulta();
+    	    	break;
     		
-    		case 3:
-    		deletar();
-    		break;
+    	    	case 3:
+    	    	deletar();
+    	    	break;
+    	    	
+    	    	case 4:
+    	    	printf("\n\t Obrigado por utilizar o sistema!\n\n");	
+    	    	printf("________________________________________________________________________________________________\n\n");
+    	    	return 0;
+    	    	break;
     		
-    		case 4:
-    		printf("\n\t Obrigado por utilizar o sistema!\n\n");	
-    		printf("________________________________________________________________________________________________\n\n");
-    		return 0;
-    		break;
-    		
-    		default: //Default pois a mensagem irá aparecer somente caso nenhum dos casos acima ocorrer
-    		printf("Essa opção não está disponível!\n");
-    		system("pause");
-    		break;
-		} //Fim da seleção
-   }
-}
+    	    	default: //Default pois a mensagem irá aparecer somente caso nenhum dos casos acima ocorrer
+    		    printf("Essa opção não está disponível!\n");
+    	    	system("pause");
+    	    	break;
+        	} //Fim da seleção
+        }
+    }
+    
+    else
+        printf("\nSenha incorreta!\n");
+      
+ }
+
+
+
+
